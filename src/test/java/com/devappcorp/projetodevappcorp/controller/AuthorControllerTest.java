183c52e5b68223e71e82f9be9085241d09a4fe03
Mathunes <matheusantunes720@gmail.com>
2022-06-15 14:12:54 -0300

    add junit 5

---
+++ src/test/java/com/devappcorp/projetodevappcorp/controller/AuthorControllerTest.java
@@ -0,0 +1,16 @@
+package com.devappcorp.projetodevappcorp.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthorControllerTest {
+
+	@Test
+    void testGet() {
+        assertEquals("Hello JUnit 5", "Hello JUnit 5");
+    }
+	
+}
