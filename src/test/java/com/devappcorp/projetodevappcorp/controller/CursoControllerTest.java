43e0a0fe5c5406e62fdc9b0a9ba85f420aa46764
Mathunes <matheusantunes720@gmail.com>
2022-06-16 20:28:59 -0300

    add CursoControllerTest

---
+++ src/test/java/com/devappcorp/projetodevappcorp/controller/CursoControllerTest.java
@@ -0,0 +1,12 @@
+package com.devappcorp.projetodevappcorp.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class CursoControllerTest {
+
+	
+	
+}
