d62ef379c5bea721a8e7a3f179ee4dd7c5dce31e
Mathunes <matheusantunes720@gmail.com>
2022-06-16 23:05:28 -0300

    add EventoControllerTest

---
+++ src/test/java/com/devappcorp/projetodevappcorp/controller/EventoControllerTest.java
@@ -0,0 +1,10 @@
+package com.devappcorp.projetodevappcorp.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EventoControllerTest {
+
+}
